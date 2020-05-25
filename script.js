71fcb0219a1f67d48cd3f3f4192a5a082b2d4789
TranTuanMinh-rmit <s3806812@rmit.edu.vn>
2020-05-25 13:53:04 +0700

    first update

---
+++ script.js
@@ -0,0 +1,8 @@
+function myResNavFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
